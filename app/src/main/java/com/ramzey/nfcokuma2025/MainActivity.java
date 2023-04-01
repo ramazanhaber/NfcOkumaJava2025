67563a8bead0eaca6466fef3bdfb367e9d83afa5
Ramazan HABER <ramazanhaberr@gmail.com>
2023-04-01 18:34:01 +0300

    record alma eklendi

--- app/src/main/java/com/ramzey/nfcokuma2025/MainActivity.java
+++ app/src/main/java/com/ramzey/nfcokuma2025/MainActivity.java
@@ -3,6 +3,8 @@ package com.ramzey.nfcokuma2025;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.IsoDep;
 import android.nfc.tech.MifareClassic;
@@ -14,11 +16,15 @@ import android.nfc.tech.NfcF;
 import android.nfc.tech.NfcV;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -87,11 +93,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         Toast.makeText(this, "NFC OKUNDU!", Toast.LENGTH_SHORT).show();
-        //if (intent.getAction().equals(NfcAdapter.ACTION_TAG_DISCOVERED)) {
-        txtNfc.setText(ByteArrayToHexString(intent.getByteArrayExtra(NfcAdapter.EXTRA_ID)));
-        // }
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        txtNfc.setText("EXTRA_ID : "+ByteArrayToHexString(intent.getByteArrayExtra(NfcAdapter.EXTRA_ID))); // EXTRA_ID
+
+        Parcelable[] rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        NdefMessage[] messages = null;
+        if (rawMessages != null) {
+            messages = new NdefMessage[rawMessages.length];
+            for (int i = 0; i < rawMessages.length; i++) {
+                messages[i] = (NdefMessage) rawMessages[i];
+                NdefRecord[] records = messages[i].getRecords();
+                //if you are sure you have text then you don't need to test TNF
+                for(NdefRecord record: records){
+                    processRecord(record);
+                }
+            }
+        }
     }
 
+    public void processRecord(NdefRecord record) {
+
+        short tnf = record.getTnf();
+        switch (tnf) {
+
+            case NdefRecord.TNF_WELL_KNOWN: {
+                if (Arrays.equals(record.getType(), NdefRecord.RTD_TEXT)) {
+                    String yourtext = processRtdTextRecord(record.getPayload());
+                    txtNfc.setText(txtNfc.getText().toString()+"\nrecord : "+yourtext);
+                } else if (Arrays.equals(record.getType(), NdefRecord.RTD_URI)) {
+                    String yourtext = record.toUri().toString();
+                    txtNfc.setText(txtNfc.getText().toString()+"\nrecord : "+yourtext);
+                } else if (Arrays.equals(record.getType(), NdefRecord.RTD_SMART_POSTER)) {
+                   // processSmartPosterRecord(record);
+                } else {
+                    //Record is not Text or URI or Poster
+                }
+            }
+            case NdefRecord.TNF_MIME_MEDIA: {
+                if (record.toMimeType().equals("MIME/Type")) {
+                    // handle this as you want
+                } else {
+                    //Record is not our MIME
+                }
+            }
+            // you can write more cases
+            default: {
+                //unsupported NDEF Record
+            }
+        }
+    }
+
+    private String processRtdTextRecord(byte[] payload) {
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+        int languageCodeLength = payload[0] & 0063;
+
+        String text = "";
+        try {
+            text = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            Log.e("UnsupportedEncoding", e.toString());
+        }
+        return text;
+    }
 
     private String ByteArrayToHexString(byte[] inarray) {
         int i, j, in;
